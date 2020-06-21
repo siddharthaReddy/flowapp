8f911af4647a31b266d8e6e1f1629bdc01f78a77
Siddhartha <siddhartha.reddy@outlook.com>
2020-06-21 11:36:37 +0530

    Updates

---
+++ precache-manifest.cc682f8107b20012ec30b166fb08caad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef07ce119fd83ef0d0db7df3a4a3d66a",
+    "url": "/flowapp/index.html"
+  },
+  {
+    "revision": "5da143d1e3a24e5dfbe3",
+    "url": "/flowapp/static/css/main.9786500b.chunk.css"
+  },
+  {
+    "revision": "8abaca53d26c35dac754",
+    "url": "/flowapp/static/js/2.2caf8221.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flowapp/static/js/2.2caf8221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da143d1e3a24e5dfbe3",
+    "url": "/flowapp/static/js/main.f6cea3ce.chunk.js"
+  },
+  {
+    "revision": "a62a6f2f5f91a29af1d8",
+    "url": "/flowapp/static/js/runtime-main.63936e10.js"
+  }
+]);
